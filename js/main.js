1b54c1f7c032bbfc2f8442f4b49ab840503d751d
Ashleigh Nichols <ashleighekari@gmail.com>
2017-12-13 11:53:23 -0500

    modified all files, deleted json

--- js/main.js
+++ js/main.js
@@ -1,50 +1,37 @@
 var initialLocations = [{
-            name: "BAM Harvey Theater",
-            //      position: {
-            lat: 40.6884270,
-            lng: -73.9787920
+        name: "BAM Harvey Theater",
+        lat: 40.6884270,
+        long: -73.9787920
         },
-        //id: "49b6a827f964a5200e531fe3"
-    },
     {
         name: "BRIC House",
-        //        position: {
-
         lat: 40.688870,
-        lng: -73.979151
+        long: -73.979151
     },
-    //id: "4a81c28df964a5207af71fe3"
-}, {
-    name: "Atlantic Terminal",
-    //       position: {
-    lat: 40.685096,
-    lng: -73.977691
+    {
+        name: "Atlantic Terminal",
+        lat: 40.685096,
+        long: -73.977691
 },
-//id: "43fa07c9f964a520d92f1fe3"
-}, {
-    name: "Beacon\'s Closet Park Slope",
-    //        position: {
-    lat: 40.680306,
-    lng: -73.978097
+    {
+        name: "Beacon\'s Closet Park Slope",
+        lat: 40.680306,
+        long: -73.978097
 },
-//id: "4a5e13d1f964a520ffbd1fe3"
-}, {
-    name: 'Barclays Center',
-    //       position: {
-    lat: 40.682682,
-    lng: -73.975035
-},
-//id: "4b992b04f964a520726635e3"
+    {
+        name: 'Barclays Center',
+        lat: 40.682682,
+        long: -73.975035
 }
 ];
 
 
-// Global variables for strict mode.
+// Global variables for the strict mode
 var map;
 var clientID;
 var clientSecret;
 
-var Location = function (data) {
+var Location = function(data) {
     var self = this;
     this.name = data.name;
     this.lat = data.lat;
@@ -54,10 +41,7 @@ var Location = function (data) {
 
     this.visible = ko.observable(true);
 
-
-
-
-    //Infowindow for street/city info
+    // Infowindow with street and city information
     this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
         '<div class="content">' + self.street + "</div>" +
         '<div class="content">' + self.city + "</div>";
@@ -66,20 +50,20 @@ var Location = function (data) {
     this.infoWindow = new google.maps.InfoWindow({
         content: self.contentString
     });
-    //The '&V=..." is used for version control. It is a YYYYMMDD format.
-    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' + this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20170607' + '&query=' + this.name;
 
-    $.getJSON(foursquareURL).done(function (data) {
+    //Authenticating foursquare api using the clientid and key that is provided with registration
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' + this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20140609 ' + '&query=' + this.name;
+
+    $.getJSON(foursquareURL).done(function(data) {
         var results = data.response.venues[0];
 
         self.street = results.location.formattedAddress[0];
         self.city = results.location.formattedAddress[1];
 
-    }).fail(function () {
+    }).fail(function() {
         alert("There was an error with the Foursquare API call. Please refresh the page and try again.");
     });
-
-    // Infowindow containing the street and city info.
+    // Infowindow with street and city information
     this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
         '<div class="content">' + self.street + "</div>" +
         '<div class="content">' + self.city + "</div>"
@@ -87,14 +71,14 @@ var Location = function (data) {
         content: self.contentString
     });
 
-    // This sets the markers on the map.
+    // Setting markers on Map
     this.marker = new google.maps.Marker({
         position: new google.maps.LatLng(data.lat, data.long),
         map: map,
         title: data.name
     });
 
-    this.showMarker = ko.computed(function () {
+    this.showMarker = ko.computed(function() {
         if (this.visible() === true) {
             this.marker.setMap(map);
         } else {
@@ -103,7 +87,7 @@ var Location = function (data) {
         return true;
     }, this);
 
-    this.marker.addListener('click', function () {
+    this.marker.addListener('click', function() {
         self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
             '<div class="content">' + self.street + "</div>" +
             '<div class="content">' + self.city + "</div>";
@@ -113,14 +97,14 @@ var Location = function (data) {
 
         self.infoWindow.open(map, this);
 
-        // Creates the bouncing animation for the marker. 
+        // this gives the bouncing affect of the pointer on the map. 
         self.marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function () {
+        setTimeout(function() {
             self.marker.setAnimation(null);
         }, 2100);
     });
 
-    this.bounce = function (place) {
+    this.bounce = function(place) {
         google.maps.event.trigger(self.marker, 'click');
     };
 };
@@ -131,32 +115,32 @@ function AppViewModel() {
     this.searchTerm = ko.observable("");
 
     this.locationList = ko.observableArray([]);
-    //This is where the map will originate.
+    //This is where the map will originate
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
+        zoom: 15,
         center: {
             lat: 40.687074,
             lng: -73.976294
         }
     });
 
-    // foursquare api settings. 
+    // foursquare api settings used to access the page. 
     clientID = "LHIMRI5U4GYF23SHGQKRN2FPLXDNV20K0S0PWZNW12ZVCD3S";
     clientSecret = "K35I1X0SHF5KTNLVDS5MY1SRKCTGUON23N2EQUIYEZDR31ZQ";
 
-    initialLocations.forEach(function (locationItem) {
+    initialLocations.forEach(function(locationItem) {
         self.locationList.push(new Location(locationItem));
     });
-    // Filtering for search list. 
-    this.filteredList = ko.computed(function () {
+    // Filtering for search list 
+    this.filteredList = ko.computed(function() {
         var filter = self.searchTerm().toLowerCase();
         if (!filter) {
-            self.locationList().forEach(function (locationItem) {
+            self.locationList().forEach(function(locationItem) {
                 locationItem.visible(true);
             });
             return self.locationList();
         } else {
-            return ko.utils.arrayFilter(self.locationList(), function (locationItem) {
+            return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
                 var string = locationItem.name.toLowerCase();
                 var result = (string.search(filter) >= 0);
                 locationItem.visible(result);
@@ -172,7 +156,7 @@ function AppViewModel() {
 function startApp() {
     ko.applyBindings(new AppViewModel());
 }
-// If there's an error with the map this function will run.
+// This will alert the user if there is an error loading the map
 function errorHandling() {
-    alert("Google Maps failed to load the requested page. Please try again.");
+    alert("Google Maps failed to load the requested page. Please refresh the page or try again later.");
 }
